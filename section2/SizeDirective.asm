3a94e4b4a2397f3f0f90f83f1bdcad489b335ef4
stomaras <sptomaras@yahoo.gr>
2020-03-08 16:32:29 +0200

    Create SizeDirective.asm

---
+++ section2/SizeDirective.asm
@@ -0,0 +1,29 @@
+Name : SizeDirective
+
+.model small
+
+.data  
+
+    var1 db 15 
+    
+    var2 dw 151
+    
+    var3 dd 115
+    
+    var4 dq ?
+    
+    var5 dt 5
+ 
+
+.code
+
+main proc
+    
+    
+    
+    
+    
+    
+    
+    main endp
+end
