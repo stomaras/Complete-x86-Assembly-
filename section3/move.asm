ffc77ca7fb15e0e112cc9676c6066c39b450546e
stomaras <sptomaras@yahoo.gr>
2020-03-08 20:04:53 +0200

    Create move.asm

---
+++ section3/move.asm
@@ -0,0 +1,23 @@
+.model small
+
+.data
+
+    C EQU 10
+    
+    var1 db 12
+    
+    var2 dw 0a112h
+    
+.code
+
+main proc
+    
+
+    
+    mov al , 3  
+    
+    mov dh , al
+    
+    
+    main endp
+end
