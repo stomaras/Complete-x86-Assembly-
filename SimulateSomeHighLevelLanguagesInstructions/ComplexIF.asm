819ac563cba44a03993665b4725765780eb419ce
stomaras <sptomaras@yahoo.gr>
2020-03-24 21:30:11 +0200

    Create ComplexIF.asm

---
+++ SimulateSomeHighLevelLanguagesInstructions/ComplexIF.asm
@@ -0,0 +1,56 @@
+
+.model small
+
+.data
+
+ msg1 db 'enter an number between 0 and 9',10,13,'$'
+ 
+ msg2 db 10,13,'you have entered the number: $'
+ 
+ msgLuck db 10,13,' you are lucky !!!$'
+ 
+.code
+
+main proc
+    
+            mov ax,@data
+            mov ds,ax
+            
+            ;print msg1
+            mov ah,9
+            lea dx,msg1
+            int 21h
+            
+            ;user input
+            mov ah,1
+            int 21h
+            mov bl,al
+            
+            ;print the number   
+            ; mov dx,msg2 offset == lea dx,msg2
+            mov ah,9
+            mov dx,msg2 offset                 
+            int 21h
+            
+            mov ah,2
+            mov dl,bl
+            int 21h  
+            
+            ; if bl >= 3 and bl <= 7
+            cmp bl,'3'
+            jb endif
+            cmp bl,'7'
+            ja endif
+            
+            ;print lucky
+            mov ah,9
+            lea dx,msgLuck
+            int 21h
+            
+      endif:mov ah,4ch
+            int 21h
+            ret
+            
+            
+    main endp
+end
