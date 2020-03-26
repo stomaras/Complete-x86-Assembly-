09e76016e3269d617a55e6f8b8da23168b768e8b
stomaras <sptomaras@yahoo.gr>
2020-03-26 17:16:23 +0200

    Create ModelSmall.asm

---
+++ ProgamSegments/ModelSmall.asm
@@ -0,0 +1,30 @@
+.model small
+
+.data
+
+ var1 db ?
+ 
+.code
+
+main proc
+
+          mov ax,@data
+          mov ds,ax
+          
+          mov al,5
+          mov ah,6
+          
+          cmp al,ah
+          
+          ja above
+          
+          mov var1,0
+          jmp endf
+          
+    above:mov var1,1
+     endf:ret
+     
+     
+     main endp
+     
+end
