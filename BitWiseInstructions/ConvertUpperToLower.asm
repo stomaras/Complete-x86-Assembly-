1cf3c1bb7f42ec3b3abda0129f2336b0088fcfdc
stomaras <sptomaras@yahoo.gr>
2020-04-03 14:08:52 +0300

    Create ConvertUpperToLower.asm

---
+++ BitWiseInstructions/ConvertUpperToLower.asm
@@ -0,0 +1,25 @@
+.model small 
+
+.data
+
+
+
+
+.code
+
+main proc
+    
+    ;upper to lower
+    
+    mov al,'D'
+     or al,00100000b
+     
+    ;add al,20h
+    
+    ;'A'01000001 'B'01000010 'C'01000011
+    
+    ;'a'01100001 'b'01100010 'c'01100011
+    
+    
+    main endp
+end
