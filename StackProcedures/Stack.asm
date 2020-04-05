ae8fd417cc581a97a77a0f5e68cbb2e620f1cb1e
stomaras <sptomaras@yahoo.gr>
2020-04-05 20:31:02 +0300

    Create Stack.asm

---
+++ StackProcedures/Stack.asm
@@ -0,0 +1,24 @@
+.model small
+
+.data
+
+
+
+.code
+
+main proc
+    
+    mov ax,5
+    push ax
+    mov bx,3
+    push bx
+    pop cx
+    pop dx
+    
+    
+    
+    
+    
+    main endp
+
+end
