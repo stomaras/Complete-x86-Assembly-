8c7a9c7f2a78864fb22071bff992b54b3c53994c
stomaras <sptomaras@yahoo.gr>
2020-04-07 21:20:03 +0300

    Create Mul2.asm

---
+++ MultiplicationDivision/Mul2.asm
@@ -0,0 +1,23 @@
+.model small
+
+.data
+
+.code
+
+
+main proc
+    
+     mov ax,0F9B9h
+     
+     mov dx,56h
+     
+     mov bx,8
+     
+     div bx
+    
+    
+    
+    
+    
+    main endp
+end
