a19180d832b6b3606c9ef9f68aa86aaf941785c8
stomaras <sptomaras@yahoo.gr>
2020-04-07 21:31:22 +0300

    Create Mul3.asm

---
+++ MultiplicationDivision/Mul3.asm
@@ -0,0 +1,23 @@
+include C:\emu8086\inc\emu8086.inc
+
+.model small
+
+.data
+
+
+.code
+
+main proc
+    
+    mov al,50
+    mov bl,10
+    
+    mul bl
+    
+    
+    main endp
+
+DEFINE_PRINT_NUM
+DEFINE_PRINT_NUM_UNS
+
+end
